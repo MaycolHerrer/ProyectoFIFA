682bc812e1685cbc69082ba8ec2887e592e874d9
senixkeyron <senixsteelheart@gmail.com>
2024-05-20 01:22:04 -0400

    modified:   FIFA Main.py

--- FIFA Main.py
+++ FIFA Main.py
@@ -1,5 +1,5 @@
 import pandas as pd
-import matplotlib.pyplot as pltt
+import matplotlib.pyplot as plt
 
 # Cargar datos del archivo CSV
 df = pd.read_csv('fifa.csv', index_col='ID', encoding='windows-1252')
